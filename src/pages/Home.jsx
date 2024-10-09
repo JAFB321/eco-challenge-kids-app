31413591b9e03e974995389c8cc579ac27eacb6a
JAFB321 <jafb321@gmail.com>
2024-10-08 23:37:14 -0700

    fix: change home copy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ export const Home = ({setStep}) => {
      <main className=" relative container mx-auto px-4 py-10 flex justify-center items-center text-center min-h-screen" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
        <section id="home" className="my-10 max-w-2xl">
          <h2 className="star7-font text-6xl font-extrabold mb-6 animate-fadeIn">Bienvenido a EcoChallenge Kids</h2>
-         <p className="mb-10 text-lg leading-relaxed animate-fadeIn">Explora mapas, animaciones e información interactiva sobre el espacio y los últimos datos proporcionados por la NASA.</p>
+         <p className="mb-10 text-lg leading-relaxed animate-fadeIn">Explora mapas, animaciones e información interactiva sobre la tierra y los últimos datos proporcionados por la NASA.</p>
         
 
          <a onClick={() => start()} className="cursor-pointer rounded-xl text-wrap space-font text-xl font-light px-6 py-2  bg-white hover:bg-black text-black hover:text-white hover:rounded-xl transition-all duration-300 ease-out transform hover:scale-105 hover:shadow-lg">
